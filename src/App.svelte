b502cba5149bfb70d86ecadc6034d5081844c3cf
Denis Perchenko <plachenko@gmail.com>
2023-08-30 01:36:02 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import Post from "./lib/post.svelte";
+  import Postdata from "./assets/gistfile1.txt?raw";
+
+  let xPosts = [];
+  let dates = [];
+  let groupPosts = [];
+
+  onMount((e) => {
+    xPosts = [...JSON.parse(Postdata)];
+
+    xPosts.forEach((e) => {
+      e.date = new Date(e.date).toLocaleString("en-US", {
+        timeZone: "America/New_York",
+      });
+      // let utc = new Date(e.date).getTimezoneOffset() * 60000;
+      // e.date = new Date(utc + 3600000 * -5.0);
+      // console.log(e.date);
+      const date = e.date.split(",")[0];
+      if (dates[date]) {
+        dates[date] = [e, ...dates[date]];
+      } else {
+        dates[date] = [e];
+      }
+    });
+
+    let idx = 0;
+
+    for (let key in dates) {
+      groupPosts[idx] = dates[key];
+      idx++;
+    }
+  });
+</script>
+
+<main>
+  <h1>ar<span id="x">X</span>iv</h1>
+  {#each groupPosts.reverse() as group}
+    <div>
+      <div class="dateGroup">
+        <span>
+          {group[0].date.split(",")[0]}
+        </span>
+      </div>
+      {#each group as post}
+        <Post {post} />
+      {/each}
+    </div>
+  {/each}
+</main>
+
+<style>
+  #x {
+    color: #000;
+    background: linear-gradient(#222, #aaa);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -webkit-text-stroke: 2px #fff;
+    /* text-shadow: 0 0 0px #fff; */
+  }
+
+  #subhead {
+    border-bottom: 1px solid #888;
+    padding: 0 10px 5px 10px;
+  }
+  .thread {
+    border-left: 2px solid;
+  }
+  .dateGroup {
+    border-bottom: 1px solid;
+    width: 100%;
+    text-align: left;
+    position: relative;
+    margin: 30px 0;
+  }
+  .dateGroup span {
+    display: inline-block;
+    height: 20px;
+    background-color: #333;
+    position: absolute;
+    top: -12px;
+    left: 10px;
+    padding: 0px 10px;
+  }
+</style>
---
+++ src/lib/post.svelte
@@ -0,0 +1,41 @@
+<script>
+  export let post;
+  let time = new Intl.DateTimeFormat("en-US", { timeStyle: "short" }).format(
+    new Date(post.date)
+  );
+</script>
+
+<div class="post {post.thread ? 'thread' : ''}">
+  <div class="date">{time}</div>
+  <div>
+    {post.post}
+  </div>
+</div>
+
+<style>
+  .post {
+    margin: 10px 10px;
+    background-color: #555;
+    padding: 20px;
+    border-radius: 4px;
+    text-align: justify;
+    position: relative;
+  }
+  .thread {
+    border-radius: 0px;
+    margin-top: -10px !important;
+    border-top: 2px dashed #999;
+  }
+
+  .post .date {
+    position: absolute;
+    color: #999;
+    background-color: #333;
+    padding: 1px 5px 3px 5px;
+    border-radius: 0px 0px 0px 10px;
+    font-size: 10px;
+    top: 0px;
+    right: 0px;
+    margin-bottom: 10px;
+  }
+</style>
